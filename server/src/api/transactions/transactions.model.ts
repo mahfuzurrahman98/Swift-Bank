1ed49f6b0f1806a9af6ca39cf099ddde2e111506
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-28 00:58:21 +0600

    transaction model added

---
+++ server/src/api/transactions/transactions.model.ts
@@ -0,0 +1,51 @@
+import { Document, Model, Mongoose, Schema } from 'mongoose';
+import Database from '../../configs/Database';
+import { ITransaction } from '../../interfaces/transaction';
+import CustomError from '../../utils/CustomError';
+
+let db: Mongoose;
+
+try {
+    db = Database.getInstance();
+} catch (error: any) {
+    throw new CustomError(500, error.message);
+}
+
+const collectionName = 'transactions';
+let transactionModel: Model<ITransaction & Document>;
+
+try {
+    const schema = new Schema<ITransaction>(
+        {
+            userId: {
+                type: String,
+                required: true,
+            },
+            amount: {
+                type: Number,
+                required: true,
+            },
+            type: {
+                type: String,
+                required: true,
+                enum: ['deposit', 'withdraw', 'transfer'],
+            },
+            toUserId: {
+                type: String,
+            },
+            deletedAt: {
+                type: Date,
+                default: null,
+            },
+        },
+        { timestamps: true }
+    );
+
+    transactionModel =
+        db.models[collectionName] ||
+        db.model<ITransaction & Document>(collectionName, schema);
+} catch (error: any) {
+    throw new CustomError(500, error.message);
+}
+
+export default transactionModel;
