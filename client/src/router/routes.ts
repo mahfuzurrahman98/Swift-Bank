3ebd0d0005a0b3adc121edef1718bbba61f0d026
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-29 19:03:22 +0600

    routes added for each page

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,20 +1,19 @@
-import GoogleLoginCallback from '../pages/GoogleLoginCallback';
-import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
+// import GoogleLoginCallback from '../pages/GoogleLoginCallback';
 import Login from '../pages/Login';
 
+import Beneficiaries from '../pages/Beneficiaries';
 import { RouteType } from '../types';
 
 const routes: RouteType[] = [
-    { path: '/', element: Home, _protected: -1 },
     { path: '/login', element: Login, _protected: 0 },
-    {
-        path: '/auth/google/callback',
-        element: GoogleLoginCallback,
-        _protected: 0,
-    },
+    // {
+    //     path: '/auth/google/callback',
+    //     element: GoogleLoginCallback,
+    //     _protected: 0,
+    // },
     { path: '/', element: Dashboard, _protected: -1 },
-
-
+    { path: '/beneficiaries', element: Beneficiaries, _protected: -1 },
 ];
 
 export default routes;
