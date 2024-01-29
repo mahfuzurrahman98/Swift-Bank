77fb3db66d7623213a41ad3df68221c31677df4e
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-01-29 19:01:28 +0600

    types added for account and transaction

--- client/src/types.ts
+++ client/src/types.ts
@@ -21,3 +21,25 @@ export type statusType = {
     loading: boolean;
     error: null | number;
 };
+
+export type AccountType = {
+    userId: string;
+    balance: number;
+    active: boolean;
+    beneficiaries: string[];
+    createdAt: string;
+    updatedAt: string;
+    deletedAt: string | null;
+};
+
+export type TransactionType = {
+    id: string;
+    fromAccountId: string;
+    amount: number;
+    type: 'deposit' | 'withdraw' | 'transfer';
+    balance: number;
+    toAccountId?: string;
+    createdAt: string;
+    updatedAt: string;
+    deletedAt?: string | null;
+};
