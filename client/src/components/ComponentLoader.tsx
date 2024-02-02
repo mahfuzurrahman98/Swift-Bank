ed10ed07414c945097f09062ae820c2c98485601
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-02-02 16:36:04 +0600

    spacing fixed

--- client/src/components/ComponentLoader.tsx
+++ client/src/components/ComponentLoader.tsx
@@ -5,20 +5,20 @@ import Loading from './Loading';
 
 // this componment will take status and the component to load
 const ComponentLoader = ({
-  status,
-  component,
+    status,
+    component,
 }: {
-  status: statusType;
-  component: ReactNode;
+    status: statusType;
+    component: ReactNode;
 }) => {
-  // console.log(status);
-  return status.loading ? (
-    <Loading />
-  ) : status.error ? (
-    <Error code={status.error} />
-  ) : (
-    component
-  );
+    // console.log(status);
+    return status.loading ? (
+        <Loading />
+    ) : status.error ? (
+        <Error code={status.error} />
+    ) : (
+        component
+    );
 };
 
 export default ComponentLoader;
