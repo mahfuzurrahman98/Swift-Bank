f344136634f71ea17e4d4b1cf970ab7c2b7c283d
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-04-23 12:10:43 +0600

    Add user profile route and format code with double quotes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,41 +1,50 @@
-import { Router } from 'express';
-import accountsHandlers from './api/accoutns/account.handlers';
-import usersHandlers from './api/users/user.handlers';
-import Auth from './utils/Auth';
+import { Router } from "express";
+import accountsHandlers from "./api/accoutns/account.handlers";
+import usersHandlers from "./api/users/user.handlers";
+import Auth from "./utils/Auth";
 
 const router = Router();
 const authMiddleware = Auth.isAuthenticated;
 
 // users routes
-router.get('/users', usersHandlers.getAll); // get all users
+router.get("/users", usersHandlers.getAll); // get all users
 
 // users auth routes
-router.post('/users', usersHandlers.create); // create new user
-router.post('/users/login', usersHandlers.login); // login user
-router.post('/users/google-login', usersHandlers.googleOAuthLogin); // login user with google
-router.post('/users/refresh-token', usersHandlers.refreshToken); // refresh token
-router.post('/users/logout', usersHandlers.logout); // logout user
+router.post("/users", usersHandlers.create); // create new user
+router.post("/users/login", usersHandlers.login); // login user
+router.post("/users/google-login", usersHandlers.googleOAuthLogin); // login user with google
+router.post("/users/refresh-token", usersHandlers.refreshToken); // refresh token
+router.get("/users/profile", usersHandlers.profile); // get profile
+router.post("/users/logout", usersHandlers.logout); // logout user
 
 // accounts routes
-router.get('/accounts', authMiddleware, accountsHandlers.getAccount);
-router.post('/accounts/deposit', authMiddleware, accountsHandlers.deposit); // deposit
-router.post('/accounts/withdraw', authMiddleware, accountsHandlers.withdraw); // withdraw
-router.post('/accounts/transfer', authMiddleware, accountsHandlers.transfer); // transfer
+router.get("/accounts", authMiddleware, accountsHandlers.getAccount);
+router.post("/accounts/deposit", authMiddleware, accountsHandlers.deposit); // deposit
+router.post("/accounts/withdraw", authMiddleware, accountsHandlers.withdraw); // withdraw
+router.post("/accounts/transfer", authMiddleware, accountsHandlers.transfer); // transfer
 
 // beneficiaries routes
-router.get('/accounts/beneficiaries', authMiddleware, accountsHandlers.getBeneficiaries);
+router.get(
+    "/accounts/beneficiaries",
+    authMiddleware,
+    accountsHandlers.getBeneficiaries
+);
 router.post(
-    '/accounts/beneficiaries',
+    "/accounts/beneficiaries",
     authMiddleware,
     accountsHandlers.createBeneficiary
 ); // create beneficiary
 router.delete(
-    '/accounts/beneficiaries/:id',
+    "/accounts/beneficiaries/:id",
     authMiddleware,
     accountsHandlers.deleteBeneficiary
 ); // delete beneficiary
 
 // transaction routes
-router.get('/accounts/transactions', authMiddleware, accountsHandlers.getTransactions); // get all transactions
+router.get(
+    "/accounts/transactions",
+    authMiddleware,
+    accountsHandlers.getTransactions
+); // get all transactions
 
 export default router;
